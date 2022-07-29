7b1b8f2d4fc24a7d497ff1846b0abe2cce9acbcd
Amie Morales <xamiemmorales@gmail.com>
2022-07-29 10:33:17 -0500

    fixed quiz

--- app.js
+++ app.js
@@ -69,85 +69,104 @@ $(() => {
    
 
 
-   const startQuiz = () => {
+const conclusion = () => {
+    $('.next-btn-2').on('click', () => {
+        $('.question-container').show()
+        $('.three').show()
+        
+    
+       $('.setThree').show()
+       $('.lastOne').show()
+       $('.lastTwo').show()
+       $('.lastThree').show()
+       $('.lastFour').show()
+       $('.lastOne').text(answers[2].answerOne)
+       $('.lastTwo').text(answers[2].answerTwo)
+       $('.lastThree').text(answers[2].answerThree)
+       $('.lastFour').text(answers[2].answerFour)
+       
+        $('.two').hide()
+        $('.secOne').hide()
+        $('.secTwo').hide()
+        $('.secThree').hide()
+        $('.secFour').hide()
+        $('.next-btn-2').hide()
+    })   
+
+    $('.lastOne').on('click', () => {
+        $('.question-container').show()
+        $('.conclusion').show()
+
+        $('.lastOne').hide()
+        $('.lastTwo').hide()
+        $('.lastThree').hide()
+        $('.lastFour').hide()
+        $('.three').hide()
+
+    })
+
+}
+const startQuiz = () => {
     $('.start-btn').on('click', () => {
         $('.one').show()
         $('.question-container').show()
-       $('.answerOne').text(answers[0].answerOne)
-       $('.answerTwo').text(answers[0].answerTwo)
+        $('.setOne').show()
+        $('.start-btn').hide()
+        $('.answerOne').text(answers[0].answerOne)
+        $('.answerTwo').text(answers[0].answerTwo)
         $('.answerThree').text(answers[0].answerThree)
         $('.answerFour').text(answers[0].answerFour)
-        
-        
-        $('.start-btn').hide()
-        // $('.next-btn').show()
-        $('.answerThree').on('click', () =>{
+        $('.setTwo').hide()
+        $('.setThree').hide()
+
+        $('.answerThree').on('click', () => [
             $('.next-btn').show()
-        })
+        ])
 
         $('.next-btn').on('click', () => {
-            $('.two').show()
             $('.one').hide()
+            $('.two').show()
             $('.next-btn').hide()
+            $('.answerOne').hide()
+            $('.answerTwo').hide()
+            $('.answerThree').hide()
+            $('.answerFour').hide()
+
             $('.question-container').show()
-           $('.answerOne').text(answers[1].answerOne)
-           $('.answerTwo').text(answers[1].answerTwo)
-            $('.answerThree').text(answers[1].answerThree)
-            $('.answerFour').text(answers[1].answerFour)
-    
+            $('.setTwo').show()
+            $('.secOne').text(answers[1].answerOne)
+            $('.secTwo').text(answers[1].answerTwo)
+            $('.secThree').text(answers[1].answerThree)
+            $('.secFour').text(answers[1].answerFour)
+
         })
 
-        $('.answerFour').on('click', () =>{
+        $('.secFour').on('click', () => {
             $('.next-btn-2').show()
+        })
+
+      
+       
+
+    })
     
-        })
+}
+startQuiz()
+conclusion()
 
-        $('.next-btn-2').on('click', () => {
-            $('.two').hide()
-            $('.three').show()
-            $('.question-container').show()
-            $('.answerOne').text(answers[2].answerOne)
-            $('.answerTwo').text(answers[2].answerTwo)
-             $('.answerThree').text(answers[2].answerThree)
-             $('.answerFour').text(answers[2].answerFour)
 
-        })
-
-        $('.answerOne').on('click', () => {
-           $('.three').hide()
-           $('.answer-button').hide()
-           $('.controls').hide()
-           
-            $('.conclusion').show()
-        })
 
         
-    })
-   }
-   startQuiz()
+        
 
 
 
 
 
-//    answerOne()
-//    secondQuestion()
-//    answerTwo()
-//    conclusion()
 
 
 
-//   lastQuestion()
 
-//    const showQuestion = (quest, answer) => {
-//     for (i=0; i< questions.length; i++) {
-//         quest = questions.question[i]
-//         answer = questions.answers[i]
-//         const $show = $('.answer')
-//         $show.appendTo($('.question-container'))
-//     }
-// }
-//  showQuestion(quest,answer)
 
 
 
@@ -160,94 +179,7 @@ $(() => {
 
 
 
-    // const resetState = () => {
-    //     clearStatusClass(document.body)
-    //     nextButton.classList.add('hide')
-    //     while (answerButton.firstChild){
-    //         answerButton.removeChild(answerButton.firstChild)
-    //     }
-    // }
-
-    // const select = (e) => {
-    //     const selectedButton = e.target
-    //     const correct =selectedButton.dataset.correct
-
-    //     setStatusClass(document.body,correct)
-    //     Array.from(answerButton.children).forEach((button) => {
-    //         setStatusClass(button,button.dataset.correct)
-    //     })
-    //     if(shuffleQuestions.length > currentQuestionIndex +1){
-    //         nextButton.classList.remove('hide')
-    //     } else {
-    //         startButton.innerText = 'restart'
-    //         startButton.classList.remove('hide')
-    //     }
-    //     if (selectedButton.dataset = correct) {
-    //         quizScore++
-    //     }
-    //     document.getElementById('right-answers').innerText=quizScore
-    // }
-    // select()
-
-    // const setStatus = (element,correct) => {
-    //     clearStatusClass(element)
-    //     if(correct){
-    //         element.classList.add('correct')
-    //     } else {
-    //         element.classList.add('wrong')
-    //     }
-    // }
-    // setStatus()
-
-    // const clearStatus = (element) => {
-    //     element.classList.remove('correct')
-    //     element.classList.remove('wrong')
-    // }
-    // clearStatus()
-
-
-
-
-
-    // const setNextQuestion = () => {
-    //     resetState()
-    //     showQuestion(shuffleQuestions[currentQuestionIndex])
-
-    // }
-
-
-    // nextButton.addEventListener('click', () => {
-    //     currentQuestionIndex++
-    //     setNextQuestion()
-    // })
-
-
-    // const showQuestion = (question) => {
-    //     questionElement.innerText = question.question;
-    //     question.answers.forEach((answer => {
-    //         const button =document.createElement('button')
-    //         button.innerText=answer.text;
-    //         button.classList.add('btn')
-    //         if (answer.correct) {
-    //             button.dataset.correct =answer.correct
-    //         }
-    //         button.addEventListener('click', selectAnswer) 
-    //         answerButton.appendChild(button)
-    //     }))
-    // }
-
-
-
-    // const startQuiz = () => {
-    //     startButton.classList.add('hide')
-    //     shuffleQuestions= questions.sort(() => Math.random() -0.5 )
-    //     currentQuestionIndex=0
-    //     questionContainer.classList.remove('hide')
-    //     setNextQuestion()
-    //     quizScore=0
-    // }
-
-    // startButton.addEventListener('click', startQuiz())
+  
 
    
 
